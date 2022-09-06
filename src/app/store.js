d478e81fe25842846faaf5c08ad46add11f0efba
Laurent LE LARDEUX <laurent.le-lardeux@inetum.com>
2022-09-06 14:02:31 +0200

    useSelector example

--- src/app/store.js
+++ src/app/store.js
@@ -3,17 +3,12 @@ import cakeReducer from '../features/cake/cakeSlice'
 import icecreamReducer from '../features/icecream/icecreamSlice'
 import userReducer from '../features/user/userSlice'
 
-//const reduxLogger = require('redux-logger')
-
-const logger = reduxLogger.createLogger()
-
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: icecreamReducer,
         user: userReducer
-    },
-    //middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+    }
 })
 
 export default store
